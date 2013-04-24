d7ba58f89ab0acd564fd00df7c6775d412e277dd
wjl <wjlingOo@126.com>
2013-04-24 22:10:02 +0800

    Hello

---
+++ src/android/basic/lesson1/MainHelloAndroid.java
@@ -0,0 +1,13 @@
+package android.basic.lesson1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainHelloAndroid extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
